a8c8c376b36d1f9f8028343640c00d9da5534b5a
eamahma <eamahma@gmail.com>
2021-06-12 20:47:59 -0400

    pssword generator loop added

--- js/script.js
+++ js/script.js
@@ -21,10 +21,14 @@ function generatePassword() {
     var upCase;
     var numDigit;
     var specialChar;
-    var passArray ="";
+    var passArray = "";
+    var result = "";
+    var index = 0;
 
     // function to get number of characters in password
     numChar = numchar();
+     console.log(numChar);
+    // console.log(typeof(numChar));
 
     // function to check if lower case to be used in password
     lowCase = lowcase();
@@ -50,7 +54,19 @@ function generatePassword() {
         passArray = passArray.concat(special_char);
     }
 
-    return passArray;
+    // if at least one character type need to be selected
+    var condition = lowCase || upCase || numDigit || specialChar;
+    console.log(condition);
+    if (condition) {
+    // algorithm to generate password
+    // used the loop suggested in https://stackoverflow.com/questions/1497481/javascript-password-generator
+        for (var index = 0, n = passArray.length; index < numChar; index++) {
+            result += passArray.charAt(Math.floor(Math.random() * n));
+        }
+        return result;
+    } else {
+        alert("At least one character type needs to be selected");
+    }
 }
 
 function numchar() {
@@ -68,7 +84,7 @@ function numchar() {
         }
     }
 
-    return result;
+    return Number(result);
 }
 
 function lowcase() {  
